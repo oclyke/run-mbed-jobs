00de32191f55e1b83a09c9a9655c8612d31c68ed
oclyke <oclyke@gmail.com>
2020-06-12 18:27:54 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,10 @@
 echo "mbed-os-url: $1"
 echo "jobs: $2"
 
-jq --version
+for row in $(echo "${jobs}" | jq -r '.[] | @base64'); do
+    _jq() {
+     echo ${row} | base64 --decode | jq -r ${1}
+    }
+
+   echo $(_jq '.name')
+done

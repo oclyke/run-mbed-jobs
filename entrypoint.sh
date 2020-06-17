248687e75a87ec59aebe3886ff2f1181a53b9f22
oclyke <oclyke@gmail.com>
2020-06-17 08:49:49 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,28 @@
 mbed=$1
 jobs=$2
 
-echo "mbed: ${mbed}"
-echo "jobs: ${jobs}"
+# echo "mbed: ${mbed}"
+# echo "jobs: ${jobs}"
 
-echo "${jobs}" | jq -r '.[]'
+# echo "${jobs}" | jq -r '.[]'
 
 for row in $(echo "${jobs}" | jq -r '.[] | @base64'); do
     _jq() {
      echo ${row} | base64 --decode | jq -r ${1}
     }
 
-   echo $(_jq '.name')
+    name=$(_jq '.name')
+    loc=$(_jq '.loc')
+    cmd=$(_jq '.cmd')
+
+    echo "name: ${name}"
+    echo "location for job: ${loc}"
+    echo "cmd: ${cmd}"
+
+    rm -rf loc
+    mkdir -p loc
+    ls loc
+    
 done
 
 touch ./test-output.txt

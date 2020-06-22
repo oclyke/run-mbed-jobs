6814846ee76bbb439cf5a8a65e06ecc8d065c508
oclyke <oclyke@gmail.com>
2020-06-22 11:40:11 -0600

    comment out long steps to speed up development

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,19 +10,17 @@ jobs=$2
 echo "mbed_opts: '${mbed_opts}'"
 echo "jobs: '${jobs}'"
 
-# echo "${jobs}" | jq -r '.[]'
-
-mbed_url=$(echo ${mbed_opts} | jq -r '.url')
-mbed_branch=$(echo ${mbed_opts} | jq -r '.branch')
-mbed_dir=${GITHUB_WORKSPACE}/tmp/mbed-os
-echo "cloning mbed from repo: '${mbed_url}' into '${mbed_dir}'"
-mkdir -p ${mbed_dir}
-git clone ${mbed_url} ${mbed_dir}
-cd ${mbed_dir}
-echo "checking out branch: ${mbed_branch}"
-git checkout ${mbed_branch}
-pip3 install -r requirements.txt
-cd ${GITHUB_WORKSPACE}
+# mbed_url=$(echo ${mbed_opts} | jq -r '.url')
+# mbed_branch=$(echo ${mbed_opts} | jq -r '.branch')
+# mbed_dir=${GITHUB_WORKSPACE}/tmp/mbed-os
+# echo "cloning mbed from repo: '${mbed_url}' into '${mbed_dir}'"
+# mkdir -p ${mbed_dir}
+# git clone ${mbed_url} ${mbed_dir}
+# cd ${mbed_dir}
+# echo "checking out branch: ${mbed_branch}"
+# git checkout ${mbed_branch}
+# pip3 install -r requirements.txt
+# cd ${GITHUB_WORKSPACE}
 
 jobs_out="["
 
@@ -42,20 +40,20 @@ for row in $(echo ${jobs} | jq -r '.[] | @base64'); do
     echo "location for job: '${loc}'"
     echo "cmd: '${cmd}'"
 
-    job_loc=${GITHUB_WORKSPACE}/${loc}
-    rm -rf ${job_loc}
-    mkdir -p ${job_loc}
+    # job_loc=${GITHUB_WORKSPACE}/${loc}
+    # rm -rf ${job_loc}
+    # mkdir -p ${job_loc}
 
-    cd ${job_loc}
+    # cd ${job_loc}
     
-    echo "making symbolic link from '${mbed_dir}' to '${job_loc}'"
-    ln -s ${mbed_dir}
+    # echo "making symbolic link from '${mbed_dir}' to '${job_loc}'"
+    # ln -s ${mbed_dir}
 
-    mbed config root .
-    ls
-    ls mbed-os
+    # mbed config root .
+    # ls
+    # ls mbed-os
 
-    mbed ${cmd} # || true # could use this to skip errors on build and continue to build other jobs
+    # mbed ${cmd} # || true # could use this to skip errors on build and continue to build other jobs
 
     cd ${GITHUB_WORKSPACE}
 

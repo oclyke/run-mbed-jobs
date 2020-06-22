4806910a212675b04817f812ee2f335096e38750
oclyke <oclyke@gmail.com>
2020-06-22 14:58:25 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -65,8 +65,7 @@ for row in $(echo ${jobs} | jq -r '.[] | @base64'); do
     cd ${GITHUB_WORKSPACE}
 
 
-    # jobs_out='${jobs_out}{"name": ${name}, "loc": "${job_loc}", "cmd": "${cmd}"}, '
-    jobs_out="${jobs_out}concat ${job_count}, "
+    jobs_out="${jobs_out}{\"name\": ${name}, \"loc\": \"${job_loc}\", \"cmd\": \"${cmd}\"}, "
     echo ${jobs_out}
     
     job_count=$((job_count + 1))
